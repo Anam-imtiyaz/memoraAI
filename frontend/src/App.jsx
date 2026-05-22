32ebf8d60051dcef834654e0bf1dc558b55ee0b0
Anam-imtiyaz <anamimtiyaz21@gmail.com>
2026-05-22 20:51:37 +0530

    Added Spring backend and connected frontend API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
 import MemoryUpload from "./components/MemoryUpload";
 import MemoryItem from "./components/MemoryItem";
 
 function App() {
+
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8081/hello")
+      .then((res) => res.text())
+      .then((data) => setMessage(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="min-h-screen bg-slate-200 p-8">
 
@@ -11,16 +22,18 @@ function App() {
           MemoraAI
         </h1>
 
-        <p className="text-gray-500 mt-2 mb-8">
+        <p className="text-gray-500 mt-2">
           Search your memories by meaning.
         </p>
 
-        <MemoryUpload />
+        <p className="mt-2 text-green-600">
+          {message}
+        </p>
 
+        <MemoryUpload />
         <MemoryItem />
 
       </div>
-
     </div>
   );
 }
